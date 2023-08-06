cc2afd39547efd0a3869aa1e5cfcb73d42f24cee
DivinationHW <G2301858089@Gmail.com>
2023-08-06 11:10:45 +0800

    Update

--- AnimeCharacterExtractor.py
+++ AnimeCharacterExtractor.py
@@ -9,8 +9,112 @@ import imagehash
 from time import time
 from sklearn.metrics.pairwise import cosine_similarity
 
-# 请求用户输入配置参数
-VIDEO_PATH = input('请输入视频文件的路径（如："demo.mkv"，回车使用默认参数："demo.mkv"）：') or 'demo.mkv'
+def process_video(VIDEO_PATH, TEMP_FOLDER, OUTPUT_FOLDER):
+    face_cascade = cv2.CascadeClassifier(CASCADE_PATH)
+    frame_id = 0
+    skip_counter = 0
+    last_hash = None
+    last_vector = None
+
+    if USE_FFMPEG == 1:
+        if not os.path.exists(TEMP_FOLDER):
+            os.makedirs(TEMP_FOLDER)
+
+        command = f'ffmpeg -i "{VIDEO_PATH}" -vf "select=eq(pict_type\\,I)" -vsync vfr -q:v 2 "{TEMP_FOLDER}/keyframe_%03d.png"'
+        subprocess.call(command, shell=True)
+
+        keyframes = sorted(os.listdir(TEMP_FOLDER))
+        pbar = tqdm(total=len(keyframes), desc='处理帧中')
+
+        for keyframe in keyframes:
+            frame = cv2.imread(os.path.join(TEMP_FOLDER, keyframe))
+            if frame is None:
+                break
+
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            faces = face_cascade.detectMultiScale(gray)
+
+            if len(faces) > 0:
+                if DETECTION_MODE == 0:
+                    if skip_counter > 0:
+                        skip_counter -= 1
+                        continue
+                    skip_counter = SKIP_FRAMES
+                elif DETECTION_MODE == 1:
+                    vector = np.array(frame).flatten()
+                    if last_vector is not None and cosine_similarity([vector], [last_vector])[0][0] > COSINE_THRESHOLD:
+                        continue
+                    last_vector = vector
+                elif DETECTION_MODE == 2:
+                    hash = imagehash.phash(Image.fromarray(frame))
+                    if last_hash is not None and abs(hash - last_hash) < HASH_DIFF_THRESHOLD:
+                        continue
+                    last_hash = hash
+
+                if DRAW_RED_BOX == 1:
+                    for (x, y, w, h) in faces:
+                        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), RED_BOX_THICKNESS)
+
+                cv2.imwrite(os.path.join(OUTPUT_FOLDER, f'output_frame_{frame_id}.jpg'), frame, [cv2.IMWRITE_JPEG_QUALITY, JPEG_QUALITY])
+                frame_id += 1
+
+            pbar.update(1)
+
+        if KEEP_TEMP_FOLDER == 0:
+            shutil.rmtree(TEMP_FOLDER)
+
+        pbar.close()
+
+    else:
+        cap = cv2.VideoCapture(VIDEO_PATH)
+        total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        cap.release()
+
+        cap = cv2.VideoCapture(VIDEO_PATH)
+        if not cap.isOpened():
+            print(f'无法打开视频：{VIDEO_PATH}')
+            exit()
+
+        pbar = tqdm(total=total_frames, desc='处理帧中')
+
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                break
+
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            faces = face_cascade.detectMultiScale(gray)
+
+            if len(faces) > 0:
+                if DETECTION_MODE == 0:
+                    if skip_counter > 0:
+                        skip_counter -= 1
+                        continue
+                    skip_counter = SKIP_FRAMES
+                elif DETECTION_MODE == 1:
+                    vector = np.array(frame).flatten()
+                    if last_vector is not None and cosine_similarity([vector], [last_vector])[0][0] > COSINE_THRESHOLD:
+                        continue
+                    last_vector = vector
+                elif DETECTION_MODE == 2:
+                    hash = imagehash.phash(Image.fromarray(frame))
+                    if last_hash is not None and abs(hash - last_hash) < HASH_DIFF_THRESHOLD:
+                        continue
+                    last_hash = hash
+
+                if DRAW_RED_BOX == 1:
+                    for (x, y, w, h) in faces:
+                        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), RED_BOX_THICKNESS)
+
+                cv2.imwrite(os.path.join(OUTPUT_FOLDER, f'output_frame_{frame_id}.jpg'), frame, [cv2.IMWRITE_JPEG_QUALITY, JPEG_QUALITY])
+                frame_id += 1
+
+            pbar.update(1)
+
+        cap.release()
+        pbar.close()
+
+INPUT_PATH = input('请输入视频文件或文件夹的路径（如："demo.mkv"或"videos_folder"，回车使用默认参数："demo.mkv"）：') or 'demo.mkv'
 OUTPUT_FOLDER = input('请输入输出文件夹路径（如："output"，回车使用默认参数："output"）：') or 'output'
 CASCADE_PATH = input('请输入级联分类器的路径（用于人脸检测，回车使用默认参数："lbpcascade_animeface.xml"）：') or 'lbpcascade_animeface.xml'
 JPEG_QUALITY = int(input('请输入输出图像的JPEG质量（0-100，数字越大，输出图像质量越高，文件大小也会增加。回车使用默认参数：100）：') or 100)
@@ -26,154 +130,23 @@ elif DETECTION_MODE == 1:
 elif DETECTION_MODE == 2:
     HASH_DIFF_THRESHOLD = int(input('请为检测模式2输入哈希差异阈值（此参数用于判断连续帧之间的哈希差异，如果差异小于此值，则跳过帧。值越小，相似度要求越严格。回车使用默认参数：5）：') or 5)
 
-# 创建时间戳后缀的临时文件夹名称
+KEEP_TEMP_FOLDER = int(input(f'是否在任务执行后保留关键帧临时文件夹（0：否，1：是。回车使用默认参数：0）：') or 0)
+
 timestamp = int(time())
-TEMP_FOLDER = f"temp_keyframes_{timestamp}"
+TEMP_ROOT_FOLDER = f"temp_keyframes_{timestamp}"
 
-# 是否在任务执行后删除关键帧临时文件夹（0：删除，1：保留。保留临时文件夹可以用于分析关键帧提取情况。回车使用默认参数：0）
-KEEP_TEMP_FOLDER = int(input(f'是否在任务执行后保留关键帧临时文件夹（0：否，1：是。临时文件夹路径为："{TEMP_FOLDER}"。回车使用默认参数：0）：') or 0)
-
-# 如果输出文件夹不存在，创建它
-if not os.path.exists(OUTPUT_FOLDER):
-    os.makedirs(OUTPUT_FOLDER)
-
-# 加载级联分类器
-face_cascade = cv2.CascadeClassifier(CASCADE_PATH)
-
-# 初始化一些变量
-frame_id = 0
-skip_counter = 0
-last_hash = None
-last_vector = None
-
-# 是否使用ffmpeg提取关键帧
-if USE_FFMPEG == 1:
-    # 创建临时文件夹
-    if not os.path.exists(TEMP_FOLDER):
-        os.makedirs(TEMP_FOLDER)
-
-    # 使用ffmpeg提取关键帧，保存为png格式，文件名以关键帧的时间戳命名
-    command = f'ffmpeg -i "{VIDEO_PATH}" -vf "select=eq(pict_type\\,I)" -vsync vfr -q:v 2 "{TEMP_FOLDER}/keyframe_%03d.png"'
-    subprocess.call(command, shell=True)
-
-    # 获取关键帧文件列表
-    keyframes = sorted(os.listdir(TEMP_FOLDER))
-
-    # 创建一个进度条
-    pbar = tqdm(total=len(keyframes), desc='处理帧中')
-
-    # 遍历每一个关键帧
-    for keyframe in keyframes:
-        # 读取关键帧
-        frame = cv2.imread(os.path.join(TEMP_FOLDER, keyframe))
-
-        # 如果读取失败，说明已经读到了视频的结尾
-        if frame is None:
-            break
-
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(gray)
-
-        # 如果检测到人物，根据DETECTION_MODE进行处理
-        if len(faces) > 0:
-            if DETECTION_MODE == 0:
-                if skip_counter > 0:
-                    skip_counter -= 1
-                    continue
-                skip_counter = SKIP_FRAMES
-            elif DETECTION_MODE == 1:
-                vector = np.array(frame).flatten()
-                if last_vector is not None and cosine_similarity([vector], [last_vector])[0][0] > COSINE_THRESHOLD:
-                    continue
-                last_vector = vector
-            elif DETECTION_MODE == 2:
-                hash = imagehash.phash(Image.fromarray(frame))
-                if last_hash is not None and abs(hash - last_hash) < HASH_DIFF_THRESHOLD:
-                    continue
-                last_hash = hash
-
-            # 在图像上为每个检测到的人像添加红色的边框
-            if DRAW_RED_BOX == 1:
-                for (x, y, w, h) in faces:
-                    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), RED_BOX_THICKNESS)
-
-            # 保存帧为图片
-            cv2.imwrite(os.path.join(OUTPUT_FOLDER, f'output_frame_{frame_id}.jpg'), frame, [cv2.IMWRITE_JPEG_QUALITY, JPEG_QUALITY])
-
-            frame_id += 1
-
-        # 更新进度条
-        pbar.update(1)
-
-    # 是否删除临时文件夹
-    if KEEP_TEMP_FOLDER == 0:
-        shutil.rmtree(TEMP_FOLDER)
-
-    # 关闭进度条
-    pbar.close()
-
+if os.path.isdir(INPUT_PATH):
+    videos = [os.path.join(INPUT_PATH, f) for f in os.listdir(INPUT_PATH) if f.endswith(('.mp4', '.mkv', '.avi', '.flv', '.wmv', '.mov', '.mpeg', '.webm'))]
+    for video_path in videos:
+        video_name = os.path.splitext(os.path.basename(video_path))[0]
+        TEMP_FOLDER = os.path.join(TEMP_ROOT_FOLDER, video_name)
+        video_output_folder = os.path.join(OUTPUT_FOLDER, video_name)
+        if not os.path.exists(video_output_folder):
+            os.makedirs(video_output_folder)
+        process_video(video_path, TEMP_FOLDER, video_output_folder)
 else:
-    # 计算视频的总帧数
-    cap = cv2.VideoCapture(VIDEO_PATH)
-    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    cap.release()
+    TEMP_FOLDER = os.path.join(TEMP_ROOT_FOLDER, os.path.splitext(os.path.basename(INPUT_PATH))[0])
+    process_video(INPUT_PATH, TEMP_FOLDER, OUTPUT_FOLDER)
 
-    # 读取视频
-    cap = cv2.VideoCapture(VIDEO_PATH)
-
-    # 检查是否成功打开
-    if not cap.isOpened():
-        print(f'无法打开视频：{VIDEO_PATH}')
-        exit()
-
-    # 创建一个进度条
-    pbar = tqdm(total=total_frames, desc='处理帧中')
-
-    # 遍历每一帧
-    while True:
-        # 读取一帧
-        ret, frame = cap.read()
-
-        # 如果读取失败，说明已经读到了视频的结尾
-        if not ret:
-            break
-
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(gray)
-
-        # 如果检测到人物，根据DETECTION_MODE进行处理
-        if len(faces) > 0:
-            if DETECTION_MODE == 0:
-                if skip_counter > 0:
-                    skip_counter -= 1
-                    continue
-                skip_counter = SKIP_FRAMES
-            elif DETECTION_MODE == 1:
-                vector = np.array(frame).flatten()
-                if last_vector is not None and cosine_similarity([vector], [last_vector])[0][0] > COSINE_THRESHOLD:
-                    continue
-                last_vector = vector
-            elif DETECTION_MODE == 2:
-                hash = imagehash.phash(Image.fromarray(frame))
-                if last_hash is not None and abs(hash - last_hash) < HASH_DIFF_THRESHOLD:
-                    continue
-                last_hash = hash
-
-            # 在图像上为每个检测到的人像添加红色的边框
-            if DRAW_RED_BOX == 1:
-                for (x, y, w, h) in faces:
-                    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), RED_BOX_THICKNESS)
-
-            # 保存帧为图片
-            cv2.imwrite(os.path.join(OUTPUT_FOLDER, f'output_frame_{frame_id}.jpg'), frame, [cv2.IMWRITE_JPEG_QUALITY, JPEG_QUALITY])
-
-            frame_id += 1
-
-        # 更新进度条
-        pbar.update(1)
-
-    # 释放视频
-    cap.release()
-
-    # 关闭进度条
-    pbar.close()
+if KEEP_TEMP_FOLDER == 0:
+    shutil.rmtree(TEMP_ROOT_FOLDER)
