e68fb74db7e30fbad3f46c8a3016db4906b3345f
DivinationHW <G2301858089@Gmail.com>
2023-08-06 08:06:48 +0800

    Update

--- AnimeCharacterExtractor.py
+++ AnimeCharacterExtractor.py
@@ -6,22 +6,32 @@ import shutil
 from tqdm import tqdm
 from PIL import Image
 import imagehash
+from time import time
 from sklearn.metrics.pairwise import cosine_similarity
 
 # 请求用户输入配置参数
-VIDEO_PATH = input('请输入视频文件的路径（回车使用默认参数："demo.mkv"）：') or 'demo.mkv'
-OUTPUT_FOLDER = input('请输入输出文件夹（回车使用默认参数："output"）：') or 'output'
-CASCADE_PATH = input('请输入级联分类器的路径（回车使用默认参数："lbpcascade_animeface.xml"）：') or 'lbpcascade_animeface.xml'
-JPEG_QUALITY = int(input('请输入输出图像的JPEG质量（0-100，回车使用默认参数：100）：') or 100)
+VIDEO_PATH = input('请输入视频文件的路径（如："demo.mkv"，回车使用默认参数："demo.mkv"）：') or 'demo.mkv'
+OUTPUT_FOLDER = input('请输入输出文件夹路径（如："output"，回车使用默认参数："output"）：') or 'output'
+CASCADE_PATH = input('请输入级联分类器的路径（用于人脸检测，回车使用默认参数："lbpcascade_animeface.xml"）：') or 'lbpcascade_animeface.xml'
+JPEG_QUALITY = int(input('请输入输出图像的JPEG质量（0-100，数字越大，输出图像质量越高，文件大小也会增加。回车使用默认参数：100）：') or 100)
 DETECTION_MODE = int(input('请输入人脸检测模式（0：直接在每一帧上进行人脸检测；1：计算余弦相似度，跳过相似的帧；2：使用图像哈希比较，跳过相似的帧。回车使用默认参数：0）：') or 0)
-USE_FFMPEG = int(input('是否使用ffmpeg提取关键帧进行人脸检测（0：否，1：是，默认：0）：') or 0)
+USE_FFMPEG = int(input('是否使用ffmpeg提取关键帧进行人脸检测（0：否，1：是。使用关键帧可以减少处理时间，若有ffmpeg环境，建议开启，下载地址：https://www.ffmpeg.org/。回车使用默认参数：0）：') or 0)
+DRAW_RED_BOX = int(input('是否在检测到的人像上画红色方框（0：否，1：是。若你觉得，啊？这图片哪来的人脸，可以尝试打开此项看看级联分类器是如何识别人脸的。回车使用默认参数：0）：') or 0)
+RED_BOX_THICKNESS = int(input('请输入红色方框的粗细（仅当上一项为1时有效，回车使用默认参数：10）：') or 10)
 
 if DETECTION_MODE == 0:
-    SKIP_FRAMES = int(input('请输入在检测到人脸后要跳过的帧数（回车使用默认参数：0）：') or 0)
+    SKIP_FRAMES = int(input('请输入在检测到人脸后要跳过的帧数（此参数用于在检测到人脸后跳过指定数量的帧，减少处理时间，若开启ffmpeg关键帧抓取，建议使用默认参数。回车使用默认参数：0）：') or 0)
 elif DETECTION_MODE == 1:
-    COSINE_THRESHOLD = float(input('请为检测模式1输入余弦相似度阈值（回车使用默认参数：0.95）：') or 0.95)
+    COSINE_THRESHOLD = float(input('请为检测模式1输入余弦相似度阈值（此参数用于判断连续帧之间的相似度，如果相似度高于此值，则跳过帧。值越接近1，相似度要求越严格。回车使用默认参数：0.95）：') or 0.95)
 elif DETECTION_MODE == 2:
-    HASH_DIFF_THRESHOLD = int(input('请为检测模式2输入哈希差异阈值（回车使用默认参数：5）：') or 5)
+    HASH_DIFF_THRESHOLD = int(input('请为检测模式2输入哈希差异阈值（此参数用于判断连续帧之间的哈希差异，如果差异小于此值，则跳过帧。值越小，相似度要求越严格。回车使用默认参数：5）：') or 5)
+
+# 创建时间戳后缀的临时文件夹名称
+timestamp = int(time())
+TEMP_FOLDER = f"temp_keyframes_{timestamp}"
+
+# 是否在任务执行后删除关键帧临时文件夹（0：删除，1：保留。保留临时文件夹可以用于分析关键帧提取情况。回车使用默认参数：0）
+KEEP_TEMP_FOLDER = int(input(f'是否在任务执行后保留关键帧临时文件夹（0：否，1：是。临时文件夹路径为："{TEMP_FOLDER}"。回车使用默认参数：0）：') or 0)
 
 # 如果输出文件夹不存在，创建它
 if not os.path.exists(OUTPUT_FOLDER):
@@ -39,12 +49,11 @@ last_vector = None
 # 是否使用ffmpeg提取关键帧
 if USE_FFMPEG == 1:
     # 创建临时文件夹
-    TEMP_FOLDER = "temp_keyframes"
     if not os.path.exists(TEMP_FOLDER):
         os.makedirs(TEMP_FOLDER)
 
-    # 使用ffmpeg提取关键帧，保存为jpg格式，文件名以关键帧的时间戳命名
-    command = f'ffmpeg -i "{VIDEO_PATH}" -vf "select=eq(pict_type\\,I)" -vsync vfr "{TEMP_FOLDER}/keyframe_%03d.jpg"'
+    # 使用ffmpeg提取关键帧，保存为png格式，文件名以关键帧的时间戳命名
+    command = f'ffmpeg -i "{VIDEO_PATH}" -vf "select=eq(pict_type\\,I)" -vsync vfr -q:v 2 "{TEMP_FOLDER}/keyframe_%03d.png"'
     subprocess.call(command, shell=True)
 
     # 获取关键帧文件列表
@@ -83,6 +92,11 @@ if USE_FFMPEG == 1:
                     continue
                 last_hash = hash
 
+            # 在图像上为每个检测到的人像添加红色的边框
+            if DRAW_RED_BOX == 1:
+                for (x, y, w, h) in faces:
+                    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), RED_BOX_THICKNESS)
+
             # 保存帧为图片
             cv2.imwrite(os.path.join(OUTPUT_FOLDER, f'output_frame_{frame_id}.jpg'), frame, [cv2.IMWRITE_JPEG_QUALITY, JPEG_QUALITY])
 
@@ -91,8 +105,9 @@ if USE_FFMPEG == 1:
         # 更新进度条
         pbar.update(1)
 
-    # 删除临时文件夹
-    shutil.rmtree(TEMP_FOLDER)
+    # 是否删除临时文件夹
+    if KEEP_TEMP_FOLDER == 0:
+        shutil.rmtree(TEMP_FOLDER)
 
     # 关闭进度条
     pbar.close()
@@ -143,6 +158,11 @@ else:
                 if last_hash is not None and abs(hash - last_hash) < HASH_DIFF_THRESHOLD:
                     continue
                 last_hash = hash
+
+            # 在图像上为每个检测到的人像添加红色的边框
+            if DRAW_RED_BOX == 1:
+                for (x, y, w, h) in faces:
+                    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), RED_BOX_THICKNESS)
 
             # 保存帧为图片
             cv2.imwrite(os.path.join(OUTPUT_FOLDER, f'output_frame_{frame_id}.jpg'), frame, [cv2.IMWRITE_JPEG_QUALITY, JPEG_QUALITY])
